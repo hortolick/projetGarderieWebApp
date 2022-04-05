36398c285ab92522cb18dd6038f4a9fe1c7d4e13
ortho <90649770+hortolick@users.noreply.github.com>
2022-04-05 09:39:20 -0400

    Ajout des dtos

---
+++ Models/DepenseDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace projetGarderieWebApp.Models
+{
+    public class DepenseDTO
+    {
+        public string DateTemps { get; set; }
+
+        public double Montant { get; set; }
+
+        public double MontantAdmissible { get; set; }
+
+        public DepenseDTO(string dateTemps = "", double montant = 0, double montantAdmissible = 0)
+        {
+            DateTemps = dateTemps;
+            Montant = montant;
+            MontantAdmissible = montantAdmissible;
+        }
+
+        public DepenseDTO() { }
+
+    }
+}
