e96cac6b932fc069393dc33d7675d27ce0beace0
ortho <90649770+hortolick@users.noreply.github.com>
2022-04-28 13:49:59 -0400

    la compilation est excessivement bien compilée en excès de la moyenne de compilation du dernier trimestre financier de l'année 2021 qui se situait avant l'annéee 2020 (la moyenne étant calculée avant les taxes)

---
+++ Controllers/CategorieDepenseController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using projetGarderieWebApp.Models;
+using projetGarderieWebApp.Tools;
+using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Threading.Tasks;
+
+namespace projetGarderieWebApp.Controllers
+{
+    public class CategorieDepenseController : Controller
+    {
+        /// <summary>
+        /// page principale
+        /// </summary>
+        /// <returns></returns>
+        [Route("CategorieDepense")]
+        [Route("CategorieDepense/Index")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            JsonValue listeCategorieDepensesJson = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/CategorieDepense/ObtenirListeCategorieDepense");
+            ViewBag.listeCategorieDepenses =JsonConvert.DeserializeObject<List<CategorieDepenseDTO>>(listeCategorieDepensesJson.ToString()).ToArray();
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Route("CategorieDepense/AjouterCategorieDepense")]
+        [HttpPost]
+        public async Task<IActionResult> AjouterCategorieDepense([FromForm] CategorieDepenseDTO garderieDTO)
+        {
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/CategorieDepense/AjouterCategorieDepense", garderieDTO);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
+            return RedirectToAction("Index", "CategorieDepense");
+        }
+
+
+
+        /// <summary>
+        /// Méthode de service appelé lors de l'action modifier.
+        /// Rôles de l'action : 
+        ///  -Lancer le formulaire Modifier
+        /// </summary>
+        /// <param name="nomCategorieDepense">le nom de la CategorieDepense a modifier</param>
+        /// <returns></returns>
+        [Route("CategorieDepense/FormModifier")]
+        [HttpGet]
+        public async Task<IActionResult> FormModifier([FromQuery] string descriptionCategorie)
+        {
+            try
+            {
+                if (TempData["MessageErreur"] != null)
+                    ViewBag.MessageErreur = TempData["MessageErreur"];
+                JsonValue jsonResponse = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/CategorieDepense/ObtenirCategorieDepense?descriptionCategorie=" + descriptionCategorie);
+                CategorieDepenseDTO garderie = JsonConvert.DeserializeObject<CategorieDepenseDTO>(jsonResponse.ToString());
+                return View(garderie);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        /// <summary>
+        /// Méthode de service appelé lors de l'action modifier.
+        /// Rôles de l'action : 
+        ///  -Modifier un CategorieDepense
+        /// </summary>
+        /// <param name="">la CategorieDepense a modifier</param>
+        /// <returns></returns>
+        [Route("CategorieDepense/ModifierCategorieDepense")]
+        [HttpPost]
+        public async Task<IActionResult> ModifierCategorieDepense([FromForm] CategorieDepenseDTO categorieDepenseDTO)
+        {
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/CategorieDepense/ModifierCategorieDepense", categorieDepenseDTO);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
+            return RedirectToAction("Index");
+        }
+
+        [Route("CategorieDepense/SupprimerCategorieDepense")]
+        [HttpPost]
+        public async Task<IActionResult> SupprimerCategorieDepense([FromForm] string descriptionCategorie)
+        {
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/CategorieDepense/SupprimerCategorieDepense?descriptionCategorie=" +descriptionCategorie, null);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
+            return RedirectToAction("Index", "CategorieDepense");
+        }
+
+        [Route("CategorieDepense/ViderListeCategorieDepense")]
+        [HttpPost]
+        public async Task<IActionResult> ViderListeCategorieDepense()
+        {
+            try
+            {
+                await WebAPI.Instance.PostAsync("http://" + Program.HOST + ":" + Program.PORT + "/CategorieDepense/ViderListeCategorieDepense", null);
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
+            return RedirectToAction("Index", "CategorieDepense");
+        }
+    }
+}
