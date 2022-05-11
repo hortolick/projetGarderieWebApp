32765dba9a9b53b490a7a762443d6377b8ed1c2b
ortho <90649770+hortolick@users.noreply.github.com>
2022-05-11 09:51:14 -0400

    oui

---
+++ Controllers/FinanceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using projetGarderieWebApp.Models;
+using projetGarderieWebApp.Tools;
+using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Threading.Tasks;
+
+namespace projetGarderieWebApp.Controllers
+{
+    public class FinanceController : Controller
+    {
+        [Route("Finance")]
+        [Route("Finance/Index")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                JsonValue listeGarderiesJson = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Garderie/ObtenirListeGarderie");
+                ViewBag.listeGarderies = JsonConvert.DeserializeObject<List<GarderieDTO>>(listeGarderiesJson.ToString()).ToArray();
+
+                return View();
+                
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
