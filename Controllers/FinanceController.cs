4fa140aadced1be07ea49f57d7c212aa57415734
ortho <90649770+hortolick@users.noreply.github.com>
2022-05-12 14:24:36 -0400

    yesseur

--- Controllers/FinanceController.cs
+++ Controllers/FinanceController.cs
@@ -14,13 +14,25 @@ namespace projetGarderieWebApp.Controllers
         [Route("Finance")]
         [Route("Finance/Index")]
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index([FromQuery] int annee, [FromQuery] string nomGarderie)
         {
             try
             {
+                if (annee == 0)
+                {
+                    annee = DateTime.Now.Year;
+                }
                 JsonValue listeGarderiesJson = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Garderie/ObtenirListeGarderie");
                 ViewBag.listeGarderies = JsonConvert.DeserializeObject<List<GarderieDTO>>(listeGarderiesJson.ToString()).ToArray();
+                if (nomGarderie == null)
+                {
+                    nomGarderie = ViewBag.listeGarderies[0].Nom;
+                }
 
+                ViewBag.nomGarderie = nomGarderie;
+                ViewBag.annee = annee;
+
+                //JsonValue listeFinanceJson = await WebAPI.Instance.ExecuteGetAsync("http://" + Program.HOST + ":" + Program.PORT + "/Finance/ObtenirFinance?annee=" + annee + "&nomGarderie=" + nomGarderie);
                 return View();
                 
             }
---
+++ Models/FinanceDTO.cs
@@ -0,0 +1,27 @@
+﻿
+namespace projetGarderieWebApp.Models
+{
+    public class FinanceDTO
+    {
+        public string nomGarderie { get; set; }
+
+        public string annee { get; set; }
+
+        public double depense { get; set; }
+
+        public double revenu { get; set; }
+
+        public double profit { get; set; }
+
+        public FinanceDTO(string nomGarderie, string annee, double depense, double revenu, double profit)
+        {
+            this.nomGarderie = nomGarderie;
+            this.annee = annee;
+            this.depense = depense;
+            this.revenu = revenu;
+            this.profit = profit;
+        }
+
+        public FinanceDTO() { }
+    }
+}
